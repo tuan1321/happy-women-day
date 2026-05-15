a715b656a55194981fd105011a2d9c88b4cf1c18
tuan1321 <73768184+tuan1321@users.noreply.github.com>
2026-05-15 15:52:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+// onload = () => {
+//     const c = setTimeout(() => {
+//       document.body.classList.remove("not-loaded");
+//       clearTimeout(c);
+//     }, 1000);
+//   };
